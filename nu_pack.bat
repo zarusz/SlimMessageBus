a8ed8d80027b29a07440db4ccc84a96ac376d333
Tomasz Maruszak <maruszaktomasz@gmail.com>
2016-05-21 17:35:26 +0200

    Adding artifact for CI. Fixing build script.

--- nu_pack.bat
+++ nu_pack.bat
@@ -3,7 +3,8 @@ set csproj_config=Release
 set csproj_platform=AnyCPU 
 
 echo Cleaning dist folder %dist_folder%
-del /S /Q %dist_folder%\*
+rmdir /S /Q %dist_folder%
+mkdir %dist_folder%
 
 nuget pack ./SlimMessageBus/SlimMessageBus.csproj -OutputDirectory ./%dist_folder% -Prop Configuration=%csproj_config% -Prop Platform=%csproj_platform% -IncludeReferencedProjects
 nuget pack ./SlimMessageBus.Core/SlimMessageBus.Core.csproj -OutputDirectory ./%dist_folder% -Prop Configuration=%csproj_config% -Prop Platform=%csproj_platform% -IncludeReferencedProjects
