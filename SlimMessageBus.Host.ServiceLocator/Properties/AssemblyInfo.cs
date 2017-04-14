b8e74942ae2658057dae72e02ad8af5556d0cc5d
Tomasz Maruszak <maruszaktomasz@gmail.com>
2017-04-14 23:29:22 +0200

    Dropping build number from version names.

--- SlimMessageBus.Host.ServiceLocator/Properties/AssemblyInfo.cs
+++ SlimMessageBus.Host.ServiceLocator/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SlimMessageBus.Host.ServiceLocator")]
-[assembly: AssemblyDescription("Extension to SlimMessageBus that resolves message consumers from the ServiceLocator")]
+[assembly: AssemblyDescription("Extension to SlimMessageBus that resolves message consumers from CommonServiceLocator")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("zarusz")]
 [assembly: AssemblyProduct("SlimMessageBus.Host.ServiceLocator")]
-[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.6.*")]
+[assembly: AssemblyVersion("0.9.10")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
