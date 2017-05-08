bad5b13f1bc829fdb3a0d3a8eb98b25b88d09fc2
Tomasz Maruszak <maruszaktomasz@gmail.com>
2017-05-08 22:49:24 +0200

    Update versions

--- nu_pack.bat
+++ nu_pack.bat
@@ -10,6 +10,7 @@ nuget pack ./SlimMessageBus/SlimMessageBus.csproj -OutputDirectory ./%dist_folde
 nuget pack ./SlimMessageBus.Host/SlimMessageBus.Host.csproj -OutputDirectory ./%dist_folder% -Prop Configuration=%csproj_config% -Prop Platform=%csproj_platform% -IncludeReferencedProjects
 nuget pack ./SlimMessageBus.Host.Serialization.Json/SlimMessageBus.Host.Serialization.Json.csproj -OutputDirectory ./%dist_folder% -Prop Configuration=%csproj_config% -Prop Platform=%csproj_platform% -IncludeReferencedProjects
 nuget pack ./SlimMessageBus.Host.ServiceLocator/SlimMessageBus.Host.ServiceLocator.csproj -OutputDirectory ./%dist_folder% -Prop Configuration=%csproj_config% -Prop Platform=%csproj_platform% -IncludeReferencedProjects
+nuget pack ./SlimMessageBus.Host.Autofac/SlimMessageBus.Host.Autofac.csproj -OutputDirectory ./%dist_folder% -Prop Configuration=%csproj_config% -Prop Platform=%csproj_platform% -IncludeReferencedProjects
 nuget pack ./SlimMessageBus.Host.Kafka/SlimMessageBus.Host.Kafka.csproj -OutputDirectory ./%dist_folder% -Prop Configuration=%csproj_config% -Prop Platform=%csproj_platform% -IncludeReferencedProjects
 
 rem nuget pack ./SlimMessageBus.Core/SlimMessageBus.Core.csproj -OutputDirectory ./%dist_folder% -Prop Configuration=%csproj_config% -Prop Platform=%csproj_platform% -IncludeReferencedProjects
