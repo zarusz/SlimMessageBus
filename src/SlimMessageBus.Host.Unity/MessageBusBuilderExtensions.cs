24e797ababd113f938adf5d864d60f0458f292a4
strukachev <disek_s@hotmail.com>
2018-02-22 17:31:51 +0700

    Unity plugin
    Signed-off-by: Denis Strukachev <disek_s@hotmail.com>

---
+++ src/SlimMessageBus.Host.Unity/MessageBusBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using SlimMessageBus.Host.Config;
+
+namespace SlimMessageBus.Host.Unity
+{
+    public static class MessageBusBuilderExtensions
+    {
+        public static MessageBusBuilder WithDependencyResolverAsUnity(this MessageBusBuilder builder)
+        {
+            return builder.WithDependencyResolver(new UnityMessageBusDependencyResolver());
+        }
+    }
+}
