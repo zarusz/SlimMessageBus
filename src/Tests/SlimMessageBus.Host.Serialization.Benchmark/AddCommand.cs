73ff297ea0a759dc1bc632eeefb19582c76f980a
Tomasz Maruszak <maruszaktomasz@gmail.com>
2020-02-04 23:50:58 +0100

    Adding serialization benchmark

---
+++ src/Tests/SlimMessageBus.Host.Serialization.Benchmark/AddCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace SlimMessageBus.Host.Serialization.Benchmark
+{
+    public class AddCommand
+    {
+        public string OperationId { get; set; }
+        public int Left { get; set; }
+        public int Right { get; set; }
+    }
+}
---
+++ src/Tests/SlimMessageBus.Host.Serialization.Benchmark/Program.cs
@@ -0,0 +1,9 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace SlimMessageBus.Host.Serialization.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+    }
+}
