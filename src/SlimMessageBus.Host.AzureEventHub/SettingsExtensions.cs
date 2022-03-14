dbbf61a3be6ad271236c7b5b3a96b8c232faabde
Tomasz Maruszak <maruszaktomasz@gmail.com>
2022-03-14 10:46:38 +0100

    [Host.AzureEventHub] Partition key provider should not be mandatory
    Signed-off-by: Tomasz Maruszak <maruszaktomasz@gmail.com>

--- src/SlimMessageBus.Host.AzureEventHub/SettingsExtensions.cs
+++ src/SlimMessageBus.Host.AzureEventHub/SettingsExtensions.cs
@@ -8,16 +8,16 @@
         private const string GroupKey = "Eh_Group";
         private const string KeyProviderKey = "Eh_KeyProvider";
 
-        public static void SetGroup(this AbstractConsumerSettings consumerSettings, string group) 
+        public static void SetGroup(this AbstractConsumerSettings consumerSettings, string group)
             => consumerSettings.Properties[GroupKey] = group;
 
-        public static string GetGroup(this AbstractConsumerSettings consumerSettings) 
+        public static string GetGroup(this AbstractConsumerSettings consumerSettings)
             => consumerSettings.Properties[GroupKey] as string;
 
-        public static void SetKeyProvider(this ProducerSettings producerSettings, Func<object, string> keyProvider) 
+        public static void SetKeyProvider(this ProducerSettings producerSettings, Func<object, string> keyProvider)
             => producerSettings.Properties[KeyProviderKey] = keyProvider;
 
-        public static Func<object, string> GetKeyProvider(this ProducerSettings producerSettings) 
-            => producerSettings.Properties[KeyProviderKey] as Func<object, string>;
+        public static Func<object, string> GetKeyProvider(this ProducerSettings producerSettings)
+            => producerSettings.Properties.ContainsKey(KeyProviderKey) ? producerSettings.Properties[KeyProviderKey] as Func<object, string> : null;
     }
 }
