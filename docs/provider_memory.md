7edcb3872a7181bc6f3b05d6c52b5f56a1b6ae55
Tomasz Maruszak <maruszaktomasz@gmail.com>
2022-07-23 23:10:15 +0200

    Additional Tests + benchmark section
    Signed-off-by: Tomasz Maruszak <maruszaktomasz@gmail.com>

--- docs/provider_memory.md
+++ docs/provider_memory.md
@@ -10,6 +10,7 @@ Please read the [Introduction](intro.md) before reading this provider documentat
 - [Lifecycle](#lifecycle)
   - [Blocking Publish](#blocking-publish)
   - [Per-Message DI scope](#per-message-di-scope)
+- [Benchmarks](#benchmarks)
   
 ## Introduction
 
@@ -122,3 +123,44 @@ ToDo: In the future we will want to expose a setting to make the `Publish<T>()` 
 ### Per-Message DI scope
 
 > Unlike stated for the [Introduction](intro.md) the memory bus has per-message scoped disabled by default. However, the memory bus consumer/handler would join the already ongoing DI scope. This is desired in scenarios where an external message is being handled as part of the unit of work (Kafka/ASB, etc) or an API HTTP request is being handled - the memory bus consumer/handler will be looked up in the ongoing/current DI scope.
+
+## Benchmarks
+
+The project [`SlimMessageBus.Host.Memory.Benchmark`](/src/Tests/SlimMessageBus.Host.Memory.Benchmark/) runs basic scenarios for pub/sub and request/response on the memory bus. The benchmark should provide some reference about the speed / performance of the Memory Bus.
+
+- The test includes a simple messages being produced.
+- The consumers do not do any logic - we want to test how fast can the messages flow through the bus.
+- The benchmark application uses a real life setup including dependency injection container.
+- No interceptors are being used.
+
+Pub/Sub scenario results:
+
+| Method | messageCount |           Mean |        Error |       StdDev |       Gen 0 |     Gen 1 |     Gen 2 |  Allocated |
+| ------ | ------------ | -------------: | -----------: | -----------: | ----------: | --------: | --------: | ---------: |
+| PubSub | 100          |       114.7 us |      1.75 us |      1.36 us |     13.3057 |         - |         - |      55 KB |
+| PubSub | 1000         |     1,181.5 us |      4.57 us |      3.81 us |    130.8594 |         - |         - |     540 KB |
+| PubSub | 10000        |    11,891.2 us |    116.28 us |    108.77 us |   1296.8750 |   62.5000 |   31.2500 |   5,491 KB |
+| PubSub | 100000       |   117,676.6 us |  1,641.94 us |  1,455.54 us |  12800.0000 |  600.0000 |  600.0000 |  54,394 KB |
+| PubSub | 1000000      | 1,204,072.3 us | 17,743.36 us | 18,221.12 us | 128000.0000 | 3000.0000 | 3000.0000 | 539,825 KB |
+
+> Pub/Sub rate is 830515 messages/s on the tested machine.
+
+Request/Response scenario results:
+
+| Method          | messageCount |           Mean |        Error |       StdDev |       Gen 0 |      Gen 1 |     Gen 2 |    Allocated |
+| --------------- | ------------ | -------------: | -----------: | -----------: | ----------: | ---------: | --------: | -----------: |
+| RequestResponse | 100          |       215.6 us |      2.78 us |      2.46 us |     28.0762 |     0.2441 |         - |       115 KB |
+| RequestResponse | 1000         |     2,119.3 us |     30.73 us |     25.66 us |    277.3438 |    35.1563 |         - |     1,141 KB |
+| RequestResponse | 10000        |    24,601.0 us |    221.66 us |    185.10 us |   2000.0000 |   968.7500 |  468.7500 |    11,507 KB |
+| RequestResponse | 100000       |   278,643.3 us |  5,465.46 us |  8,509.06 us |  19000.0000 |  6000.0000 | 2000.0000 |   114,559 KB |
+| RequestResponse | 1000000      | 2,753,348.9 us | 25,449.35 us | 23,805.34 us | 186000.0000 | 51000.0000 | 6000.0000 | 1,141,392 KB |
+
+> Request/Response rate is 363194 messages/s on the tested machine.
+
+The test results are for the following environment:
+
+```text
+BenchmarkDotNet=v0.13.1, OS=Windows 10.0.22000
+Intel Core i7-8550U CPU 1.80GHz (Kaby Lake R), 1 CPU, 8 logical and 4 physical cores
+.NET SDK=6.0.302
+```
--- src/Tests/SlimMessageBus.Host.Memory.Test/MemoryMessageBusTests.cs
+++ src/Tests/SlimMessageBus.Host.Memory.Test/MemoryMessageBusTests.cs
@@ -18,26 +18,30 @@ using Xunit;
 public class MemoryMessageBusTests
 {
     private readonly Lazy<MemoryMessageBus> _subject;
-    private readonly MessageBusSettings _settings = new();
+    private readonly MessageBusSettings _settings;
+    private readonly MessageBusBuilder _builder;
     private readonly MemoryMessageBusSettings _providerSettings = new();
     private readonly Mock<IDependencyResolver> _dependencyResolverMock = new();
     private readonly Mock<IMessageSerializer> _messageSerializerMock = new();
 
     public MemoryMessageBusTests()
     {
+        _builder = MessageBusBuilder.Create()
+            .WithDependencyResolver(_dependencyResolverMock.Object)
+            .WithSerializer(_messageSerializerMock.Object)
+            .ExpectRequestResponses(x =>
+            {
+                x.ReplyToTopic("responses");
+                x.DefaultTimeout(TimeSpan.FromHours(1));
+            });
+
+        _settings = _builder.Settings;
+
         _dependencyResolverMock.Setup(x => x.Resolve(It.IsAny<Type>())).Returns((Type t) =>
         {
             if (t.IsGenericType && t.GetGenericTypeDefinition() == typeof(IEnumerable<>)) return Enumerable.Empty<object>();
             return null;
         });
-
-        _settings.DependencyResolver = _dependencyResolverMock.Object;
-        _settings.Serializer = _messageSerializerMock.Object;
-        _settings.RequestResponse ??= new RequestResponseSettings
-        {
-            Timeout = TimeSpan.FromHours(1),
-            Path = "responses"
-        };
 
         _messageSerializerMock
             .Setup(x => x.Serialize(It.IsAny<Type>(), It.IsAny<object>()))
@@ -49,44 +53,27 @@ public class MemoryMessageBusTests
         _subject = new Lazy<MemoryMessageBus>(() => new MemoryMessageBus(_settings, _providerSettings));
     }
 
-    private static ProducerSettings Producer(Type messageType, string defaultTopic) => new()
-    {
-        MessageType = messageType,
-        DefaultPath = defaultTopic
-    };
-
-    private static ConsumerSettings Consumer(Type messageType, string topic, Type consumerType) 
-        => new ConsumerBuilder<object>(new MessageBusSettings(), messageType).Topic(topic).WithConsumer(consumerType).ConsumerSettings;
-
-    private static ConsumerSettings Handler<TRequest, TResponse, THandler>(string topic) where THandler : IRequestHandler<TRequest, TResponse>
-        => new HandlerBuilder<TRequest, TResponse>(new MessageBusSettings(), typeof(TRequest)).Topic(topic).WithHandler<THandler>().ConsumerSettings;
-
-    [Fact]
-    public void When_Create_Given_MessageSerializationDisabled_And_NoSerializerProvided_Then_NoException()
+    [Theory]
+    [InlineData(false)]
+    [InlineData(true)]
+    public void When_Create_Given_MessageSerializationEnabled_And_NoSerializerProvided_Then_ThrowsExceptionOrNot(bool serializationEnabled)
     {
         // arrange
         _settings.Serializer = null;
-        _providerSettings.EnableMessageSerialization = false;
-
-        // act
-        Action act = () => { var _ = _subject.Value; };
-
-        // assert            
-        act.Should().NotThrow<ConfigurationMessageBusException>();
-    }
-
-    [Fact]
-    public void When_Create_Given_MessageSerializationEnabled_And_NoSerializerProvided_Then_ThrowsException()
-    {
-        // arrange
-        _settings.Serializer = null;
-        _providerSettings.EnableMessageSerialization = true;
+        _providerSettings.EnableMessageSerialization = serializationEnabled;
 
         // act
         Action act = () => { var _ = _subject.Value; };
 
         // assert          
-        act.Should().Throw<ConfigurationMessageBusException>();
+        if (serializationEnabled)
+        {
+            act.Should().Throw<ConfigurationMessageBusException>();
+        }
+        else
+        {
+            act.Should().NotThrow<ConfigurationMessageBusException>();
+        }
     }
 
     [Theory]
@@ -99,11 +86,11 @@ public class MemoryMessageBusTests
         const string topicA2 = "topic-a-2";
         const string topicB = "topic-b";
 
-        _settings.Producers.Add(Producer(typeof(SomeMessageA), topicA));
-        _settings.Producers.Add(Producer(typeof(SomeMessageB), topicB));
-        _settings.Consumers.Add(Consumer(typeof(SomeMessageA), topicA, typeof(SomeMessageAConsumer)));
-        _settings.Consumers.Add(Consumer(typeof(SomeMessageA), topicA2, typeof(SomeMessageAConsumer2)));
-        _settings.Consumers.Add(Consumer(typeof(SomeMessageB), topicB, typeof(SomeMessageBConsumer)));
+        _builder.Produce<SomeMessageA>(x => x.DefaultTopic(topicA));
+        _builder.Produce<SomeMessageB>(x => x.DefaultTopic(topicB));
+        _builder.Consume<SomeMessageA>(x => x.Topic(topicA).WithConsumer<SomeMessageAConsumer>());
+        _builder.Consume<SomeMessageA>(x => x.Topic(topicA2).WithConsumer<SomeMessageAConsumer2>());
+        _builder.Consume<SomeMessageB>(x => x.Topic(topicB).WithConsumer<SomeMessageBConsumer>());
 
         var aConsumerMock = new Mock<SomeMessageAConsumer>();
         var aConsumer2Mock = new Mock<SomeMessageAConsumer2>();
@@ -151,9 +138,9 @@ public class MemoryMessageBusTests
 
         const string topic = "topic-a";
 
-        _settings.Producers.Add(Producer(typeof(SomeMessageA), topic));
-        _settings.Consumers.Add(Consumer(typeof(SomeMessageA), topic, typeof(SomeMessageAConsumer)));
-        _settings.IsMessageScopeEnabled = true;
+        _builder.Produce<SomeMessageA>(x => x.DefaultTopic(topic));
+        _builder.Consume<SomeMessageA>(x => x.Topic(topic).WithConsumer<SomeMessageAConsumer>());
+        _builder.PerMessageScopeEnabled(true);
 
         _providerSettings.EnableMessageSerialization = false;
 
@@ -189,13 +176,9 @@ public class MemoryMessageBusTests
 
         const string topic = "topic-a";
 
-        _settings.Producers.Add(Producer(typeof(SomeMessageA), topic));
-
-        var consumerSettings = Consumer(typeof(SomeMessageA), topic, typeof(SomeMessageAConsumer));
-        consumerSettings.IsDisposeConsumerEnabled = true;
-
-        _settings.Consumers.Add(consumerSettings);
-        _settings.IsMessageScopeEnabled = false;
+        _builder.Produce<SomeMessageA>(x => x.DefaultTopic(topic));
+        _builder.Consume<SomeMessageA>(x => x.Topic(topic).WithConsumer<SomeMessageAConsumer>().DisposeConsumerEnabled(true));
+        _builder.PerMessageScopeEnabled(false);
 
         _providerSettings.EnableMessageSerialization = false;
 
@@ -233,13 +216,9 @@ public class MemoryMessageBusTests
 
         const string topic = "topic-a";
 
-        _settings.Producers.Add(Producer(typeof(SomeMessageA), topic));
-
-        var consumerSettings = Consumer(typeof(SomeMessageA), topic, typeof(SomeMessageAConsumer));
-        consumerSettings.IsDisposeConsumerEnabled = true;
-
-        _settings.Consumers.Add(consumerSettings);
-        _settings.IsMessageScopeEnabled = isMessageScopeEnabled;
+        _builder.Produce<SomeMessageA>(x => x.DefaultTopic(topic));
+        _builder.Consume<SomeMessageA>(x => x.Topic(topic).WithConsumer<SomeMessageAConsumer>().DisposeConsumerEnabled(true));
+        _builder.PerMessageScopeEnabled(isMessageScopeEnabled);
 
         _providerSettings.EnableMessageSerialization = false;
 
@@ -284,13 +263,9 @@ public class MemoryMessageBusTests
 
         const string topic = "topic-a";
 
-        _settings.Producers.Add(Producer(typeof(SomeMessageA), topic));
-
-        var consumer1Settings = Consumer(typeof(SomeMessageA), topic, typeof(SomeMessageAConsumer));
-        var consumer2Settings = Consumer(typeof(SomeMessageA), topic, typeof(SomeMessageAConsumer2));
-
-        _settings.Consumers.Add(consumer1Settings);
-        _settings.Consumers.Add(consumer2Settings);
+        _builder.Produce<SomeMessageA>(x => x.DefaultTopic(topic));
+        _builder.Consume<SomeMessageA>(x => x.Topic(topic).WithConsumer<SomeMessageAConsumer>());
+        _builder.Consume<SomeMessageA>(x => x.Topic(topic).WithConsumer<SomeMessageAConsumer2>());
 
         var m = new SomeMessageA(Guid.NewGuid());
 
@@ -317,34 +292,33 @@ public class MemoryMessageBusTests
     }
 
     [Fact]
-    public async Task When_Send_Given_AConsumersAndHandlerOnSameTopic_Then_BothAreInvokedAndHandlerResponseIsUsed()
+    public async Task When_Send_Given_AConsumersAndHandlerOnSameTopic_Then_BothAreInvoked_And_ConsumerIsFirst_And_HandlerResponseIsUsed_And_InterceptorsAreLookedUp()
     {
         const string topic = "topic-a";
 
         var m = new SomeRequest(Guid.NewGuid());
 
-        var consumer1Mock = new Mock<SomeRequestConsumer>();
-        consumer1Mock.Setup(x => x.OnHandle(m, topic)).CallBase();
+        var sequenceOfConsumption = new MockSequence();
 
-        var consumer2Mock = new Mock<SomeRequestHandler>();
-        consumer2Mock.Setup(x => x.OnHandle(m, topic)).CallBase();
+        var consumer1Mock = new Mock<SomeRequestConsumer>(MockBehavior.Strict);
+        consumer1Mock.InSequence(sequenceOfConsumption).Setup(x => x.OnHandle(m, topic)).CallBase();
+
+        var consumer2Mock = new Mock<SomeRequestHandler>(MockBehavior.Strict);
+        consumer2Mock.InSequence(sequenceOfConsumption).Setup(x => x.OnHandle(m, topic)).CallBase();
 
         _dependencyResolverMock.Setup(x => x.Resolve(typeof(SomeRequestConsumer))).Returns(() => consumer1Mock.Object);
         _dependencyResolverMock.Setup(x => x.Resolve(typeof(SomeRequestHandler))).Returns(() => consumer2Mock.Object);
 
-        _settings.Producers.Add(Producer(typeof(SomeRequest), topic));
-
-        var consumer1Settings = Consumer(typeof(SomeRequest), topic, typeof(SomeRequestConsumer));
-        var consumer2Settings = Handler<SomeRequest, SomeResponse, SomeRequestHandler>(topic);
-
-        _settings.Consumers.Add(consumer1Settings);
-        _settings.Consumers.Add(consumer2Settings);
+        _builder.Produce<SomeRequest>(x => x.DefaultTopic(topic));
+        _builder.Consume<SomeRequest>(x => x.Topic(topic).WithConsumer<SomeRequestConsumer>());
+        _builder.Handle<SomeRequest, SomeResponse>(x => x.Topic(topic).WithHandler<SomeRequestHandler>());
 
         // act
         var response = await _subject.Value.Send(m);
 
         // assert
         response.Should().NotBeNull();
+        response.Id.Should().Be(m.Id);
 
         // current scope is not changed
         _dependencyResolverMock.Verify(x => x.Resolve(typeof(ILoggerFactory)), Times.Once);
@@ -357,11 +331,55 @@ public class MemoryMessageBusTests
         _dependencyResolverMock.Verify(x => x.Resolve(typeof(IEnumerable<IRequestHandlerInterceptor<SomeRequest, SomeResponse>>)), Times.Once);
         _dependencyResolverMock.VerifyNoOtherCalls();
 
-        consumer1Mock.Verify(x => x.OnHandle(m, topic), Times.Once);
-        consumer1Mock.VerifyNoOtherCalls();
-
         consumer2Mock.Verify(x => x.OnHandle(m, topic), Times.Once);
         consumer2Mock.VerifyNoOtherCalls();
+
+        consumer1Mock.Verify(x => x.OnHandle(m, topic), Times.Once);
+        consumer1Mock.VerifyNoOtherCalls();
+    }
+
+    [Fact]
+    public async Task When_Publish_Given_AConsumersThatThrowsException_Then_ExceptionIsBubblingToPublisher()
+    {
+        const string topic = "topic-a";
+
+        var m = new SomeRequest(Guid.NewGuid());
+
+        var consumerMock = new Mock<SomeRequestConsumer>();
+        consumerMock.Setup(x => x.OnHandle(m, topic)).ThrowsAsync(new ApplicationException("Bad Request"));
+
+        _dependencyResolverMock.Setup(x => x.Resolve(typeof(SomeRequestConsumer))).Returns(() => consumerMock.Object);
+
+        _builder.Produce<SomeRequest>(x => x.DefaultTopic(topic));
+        _builder.Consume<SomeRequest>(x => x.Topic(topic).WithConsumer<SomeRequestConsumer>());
+
+        // act
+        var act = () => _subject.Value.Publish(m);
+
+        // assert
+        await act.Should().ThrowAsync<ApplicationException>();
+    }
+
+    [Fact]
+    public async Task When_Send_Given_AHandlerThatThrowsException_Then_ExceptionIsBubblingToSender()
+    {
+        const string topic = "topic-a";
+
+        var m = new SomeRequest(Guid.NewGuid());
+
+        var consumerMock = new Mock<SomeRequestHandler>();
+        consumerMock.Setup(x => x.OnHandle(m, topic)).ThrowsAsync(new ApplicationException("Bad Request"));
+
+        _dependencyResolverMock.Setup(x => x.Resolve(typeof(SomeRequestHandler))).Returns(() => consumerMock.Object);
+
+        _builder.Produce<SomeRequest>(x => x.DefaultTopic(topic));
+        _builder.Handle<SomeRequest, SomeResponse>(x => x.Topic(topic).WithHandler<SomeRequestHandler>());
+
+        // act
+        var act = () => _subject.Value.Send(m);
+
+        // assert
+        await act.Should().ThrowAsync<ApplicationException>();
     }
 }
 
@@ -390,6 +408,7 @@ public class SomeMessageBConsumer : IConsumer<SomeMessageB>
 }
 
 public record SomeRequest(Guid Id) : IRequestMessage<SomeResponse>;
+
 public record SomeResponse(Guid Id);
 
 public class SomeRequestHandler : IRequestHandler<SomeRequest, SomeResponse>
