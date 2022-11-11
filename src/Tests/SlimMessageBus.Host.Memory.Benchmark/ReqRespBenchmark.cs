b2668a77700aad8a4271643ec5b2ec96b5f03849
Tomasz Maruszak <maruszaktomasz@gmail.com>
2022-11-11 14:33:23 +0100

    Extend the benchmark tests with interceptor tests
    Signed-off-by: Tomasz Maruszak <maruszaktomasz@gmail.com>

--- src/Tests/SlimMessageBus.Host.Memory.Benchmark/ReqRespBenchmark.cs
+++ src/Tests/SlimMessageBus.Host.Memory.Benchmark/ReqRespBenchmark.cs
@@ -3,55 +3,24 @@
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Order;
 using Microsoft.Extensions.DependencyInjection;
-using SlimMessageBus.Host.MsDependencyInjection;
-using System.Reflection;
+using SlimMessageBus.Host.Interceptor;
 
-[Orderer(SummaryOrderPolicy.FastestToSlowest)]
-[MemoryDiagnoser]
-public class ReqRespBenchmark : IDisposable
+public abstract class ReqRespBaseBenchmark : AbstractMemoryBenchmark
 {
-    private ServiceProvider svp;
     private readonly TestResult testResult;
-    private readonly IMessageBus bus;
 
-    public ReqRespBenchmark()
+    protected ReqRespBaseBenchmark()
     {
-        var services = new ServiceCollection();
-
-        services.AddSlimMessageBus((mbb, _) =>
-        {
-            mbb
-                .WithProviderMemory()
-                .AutoDeclareFrom(Assembly.GetExecutingAssembly());
-            //.Produce<SomeRequest>(x => x.DefaultTopic(x.MessageType.Name))
-            //.Handle<SomeRequest, SomeResponse>(x => x.Topic(x.MessageType.Name).WithHandler<SomeRequestHandler>());
-        });
-
-        services.AddSingleton<TestResult>();
-        services.AddTransient<SomeRequestHandler>();
-
-        svp = services.BuildServiceProvider();
-
-        bus = svp.GetRequiredService<IMessageBus>();
         testResult = svp.GetRequiredService<TestResult>();
     }
 
-    public void Dispose()
+    protected override void Setup(ServiceCollection services)
     {
-        if (svp != null)
-        {
-            svp.Dispose();
-            svp = null;
-        }
+        services.AddSingleton<TestResult>();
+        services.AddTransient<SomeRequestHandler>();
     }
 
-    [Benchmark]
-    [Arguments(100)]
-    [Arguments(1000)]
-    [Arguments(10000)]
-    [Arguments(100000)]
-    [Arguments(1000000)]
-    public async Task RequestResponse(int messageCount)
+    public async Task RunTest(int messageCount)
     {
         var sendRequests = Enumerable.Range(0, messageCount).Select(x => bus.Send(new SomeRequest(DateTimeOffset.Now, x)));
 
@@ -64,19 +33,87 @@ public class ReqRespBenchmark : IDisposable
     }
 }
 
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[MemoryDiagnoser]
+public class ReqRespBenchmark : ReqRespBaseBenchmark
+{
+    [Benchmark]
+    [Arguments(100)]
+    [Arguments(1000)]
+    [Arguments(10000)]
+    [Arguments(100000)]
+    [Arguments(1000000)]
+    public Task RequestResponse(int messageCount) => RunTest(messageCount);
+}
+
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[MemoryDiagnoser]
+public class ReqRespWithProducerInterceptorBenchmark : ReqRespBaseBenchmark
+{
+    protected override void Setup(ServiceCollection services)
+    {
+        base.Setup(services);
+
+        services.AddTransient<IProducerInterceptor<SomeRequest>, SomeRequestProducerInterceptor>();
+    }
+
+    [Benchmark]
+    [Arguments(100)]
+    [Arguments(1000)]
+    [Arguments(10000)]
+    [Arguments(100000)]
+    [Arguments(1000000)]
+    public Task ReqRespWithProducerInterceptor(int messageCount) => RunTest(messageCount);
+}
+
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[MemoryDiagnoser]
+public class ReqRespWithConsumerInterceptorBenchmark : ReqRespBaseBenchmark
+{
+    protected override void Setup(ServiceCollection services)
+    {
+        base.Setup(services);
+
+        services.AddTransient<IConsumerInterceptor<SomeRequest>, SomeRequestConsumerInterceptor>();
+    }
+
+    [Benchmark]
+    [Arguments(100)]
+    [Arguments(1000)]
+    [Arguments(10000)]
+    [Arguments(100000)]
+    [Arguments(1000000)]
+    public Task ReqRespWithProducerInterceptor(int messageCount) => RunTest(messageCount);
+}
+
+
 public record SomeRequest(DateTimeOffset Timestamp, long Id) : IRequestMessage<SomeResponse>;
 
 public record SomeResponse(DateTimeOffset Timestamp, long Id);
 
-public class SomeRequestHandler : IRequestHandler<SomeRequest, SomeResponse>
+public record SomeRequestHandler(TestResult TestResult) : IRequestHandler<SomeRequest, SomeResponse>
 {
-    private readonly TestResult testResult;
-
-    public SomeRequestHandler(TestResult testResult) => this.testResult = testResult;
-
     public Task<SomeResponse> OnHandle(SomeRequest request)
     {
-        testResult.OnArrived();
+        TestResult.OnArrived();
         return Task.FromResult(new SomeResponse(DateTimeOffset.Now, request.Id));
+    }
+}
+
+public record SomeRequestProducerInterceptor : IProducerInterceptor<SomeRequest>
+{
+    public Task<object> OnHandle(SomeRequest message, Func<Task<object>> next, IProducerContext context)
+    {
+        // We return immediately as we want to calculate the interceptor pipeline overhead
+        return next();
+    }
+}
+
+public record SomeRequestConsumerInterceptor : IConsumerInterceptor<SomeRequest>
+{
+    public Task OnHandle(SomeRequest message, Func<Task> next, IConsumerContext context)
+    {
+        // We return immediately as we want to calculate the interceptor pipeline overhead
+        return next();
     }
 }
