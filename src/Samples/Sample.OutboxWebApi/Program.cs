a887a2ba7df274e15117a831c2d039f2c0168e5d
Tomasz Maruszak <maruszaktomasz@gmail.com>
2024-04-01 17:05:48 +0200

    Sonar diff analysis for external PRs
    Signed-off-by: Tomasz Maruszak <maruszaktomasz@gmail.com>

--- src/Samples/Sample.OutboxWebApi/Program.cs
+++ src/Samples/Sample.OutboxWebApi/Program.cs
@@ -21,8 +21,6 @@ Secrets.Load(@"..\..\secrets.txt");
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
