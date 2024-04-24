1b74fea81334361c63524d538f5153e1382eacd3
Tomasz Maruszak <maruszaktomasz@gmail.com>
2024-04-24 23:24:19 +0200

    Error when using JsonMessageSerializer with default settings #252
    Signed-off-by: Tomasz Maruszak <maruszaktomasz@gmail.com>

--- src/Tests/SlimMessageBus.Host.Serialization.SystemTextJson.Test/JsonMessageSerializerTests.cs
+++ src/Tests/SlimMessageBus.Host.Serialization.SystemTextJson.Test/JsonMessageSerializerTests.cs
@@ -1,6 +1,5 @@
 namespace SlimMessageBus.Host.Serialization.SystemTextJson.Test;
 
-using FluentAssertions;
 
 public class JsonMessageSerializerTests
 {
@@ -28,5 +27,50 @@ public class JsonMessageSerializerTests
 
         // assert
         deserializedValue.Should().Be(expectedValue);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void When_RegisterSerializer_Then_UsesOptionsFromContainerIfAvailable(bool jsonOptionComeFromContainer)
+    {
+        // arrange
+        var mbb = MessageBusBuilder.Create();
+        mbb.AddJsonSerializer();
+
+        var services = new ServiceCollection();
+
+        var jsonOptions = new JsonSerializerOptions();
+
+        // Simulate the options have been used in an serializer already (see https://github.com/zarusz/SlimMessageBus/issues/252)
+        // Modifying options (adding converters) when the options are already in use by a serializer will throw an exception: System.InvalidOperationException : This JsonSerializerOptions instance is read-only or has already been used in serialization or deserialization.
+        JsonSerializer.SerializeToUtf8Bytes(new Dictionary<string, object>(), typeof(Dictionary<string, object>), jsonOptions);
+
+        if (jsonOptionComeFromContainer)
+        {
+            services.AddSingleton(jsonOptions);
+        }
+
+        foreach (var action in mbb.PostConfigurationActions)
+        {
+            action(services);
+        }
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        var subject = serviceProvider.GetRequiredService<JsonMessageSerializer>();
+
+        // act
+        if (jsonOptionComeFromContainer)
+        {
+            subject.Options.Should().BeSameAs(jsonOptions);
+        }
+        else
+        {
+            subject.Options.Should().NotBeSameAs(jsonOptions);
+        }
+
+        // assert
+        serviceProvider.GetService<IMessageSerializer>().Should().BeSameAs(subject);
     }
 }
