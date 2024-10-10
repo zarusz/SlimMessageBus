457b037a7be7cf8e1ec541103167ef5993d2cd1a
Tomasz Maruszak <maruszaktomasz@gmail.com>
2024-10-10 23:42:55 +0200

    Support for UUIDv7 values
    Signed-off-by: Tomasz Maruszak <maruszaktomasz@gmail.com>

--- src/SlimMessageBus.Host.Outbox/Configuration/OutboxMessageIdGenerationMode.cs
+++ src/SlimMessageBus.Host.Outbox/Configuration/OutboxMessageIdGenerationMode.cs
@@ -3,12 +3,12 @@
 public enum OutboxMessageIdGenerationMode
 {
     /// <summary>
-    /// The database is responsible for generating the message id, using a guid (NEWID()).
+    /// The database is responsible for generating the message id, using NEWID().
     /// See https://learn.microsoft.com/en-us/sql/t-sql/functions/newid-transact-sql?view=sql-server-ver16
     /// </summary>
     DatabaseGeneratedGuid,
     /// <summary>
-    /// The database is responsible for generating the message id, using a sequential guid (NEWSEQUENTIALID()).
+    /// The database is responsible for generating the message id, using NEWSEQUENTIALID().
     /// See https://learn.microsoft.com/en-us/sql/t-sql/functions/newsequentialid-transact-sql?view=sql-server-ver16
     /// </summary>
     DatabaseGeneratedSequentialGuid,
