7e27c6fa1abe8327cfa3ed79705e9096b0a6bb99
Tomasz Maruszak <maruszaktomasz@gmail.com>
2025-04-13 14:27:18 +0200

    [Host.AmazonSQS] Add SNS support
    Signed-off-by: Tomasz Maruszak <maruszaktomasz@gmail.com>

--- src/SlimMessageBus.Host.AmazonSQS/Consumer/SqsQueueConsumer.cs
+++ src/SlimMessageBus.Host.AmazonSQS/Consumer/SqsQueueConsumer.cs
@@ -1,15 +1,17 @@
 ﻿namespace SlimMessageBus.Host.AmazonSQS;
 
-public class SqsQueueConsumer(
+internal class SqsQueueConsumer(
     SqsMessageBus messageBus,
     string path,
     ISqsClientProvider clientProvider,
-    IMessageProcessor<Message> messageProcessor,
+    IMessageProcessor<SqsTransportMessageWithPayload> messageProcessor,
+    IMessageSerializer<string> messageSerializer,
     IEnumerable<AbstractConsumerSettings> consumerSettings)
     : SqsBaseConsumer(messageBus,
           clientProvider,
           path,
           messageProcessor,
+          messageSerializer,
           consumerSettings,
           messageBus.LoggerFactory.CreateLogger<SqsQueueConsumer>())
 {
