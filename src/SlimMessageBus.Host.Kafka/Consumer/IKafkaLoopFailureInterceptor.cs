5e83beacf658e9bce91b6a0fce725240608cb4bb
tmishurina <t.mishurina@reglab.ru>
2026-05-20 16:35:47 +0500

    [Host.Kafka] Add KafkaLoopFailureInterceptor
    Signed-off-by: tmishurina <t.mishurina@reglab.ru>

---
+++ src/SlimMessageBus.Host.Kafka/Consumer/IKafkaLoopFailureInterceptor.cs
@@ -0,0 +1,12 @@
+﻿namespace SlimMessageBus.Host.Kafka;
+
+using Interceptor;
+
+/// <summary>
+/// Provides a way to intercept fatal failures occurring within the Kafka consumer group loop (<see cref="KafkaGroupConsumer.ConsumerLoop"/>).
+/// These failures are typically unrecoverable errors that terminate the entire consumer group instance.
+/// </summary>
+public interface IKafkaLoopFailureInterceptor : IInterceptorWithOrder
+{
+    Task OnFailureAsync(KafkaLoopFailureContext context);
+}
---
+++ src/Tests/SlimMessageBus.Host.Kafka.Test/Consumer/KafkaGroupLoopConsumerTests.cs
@@ -0,0 +1,77 @@
+﻿namespace SlimMessageBus.Host.Kafka.Test;
+
+public class KafkaLoopGroupConsumerTests
+{
+    private readonly KafkaGroupConsumer _subject;
+    
+    private const string GROUP = "group-a";
+    
+    private const string TOPIC = "topic-a";
+    
+    private readonly Mock<IKafkaLoopFailureInterceptor> _mockFailureInterceptor;
+    
+    private readonly Exception _expectedException = new InvalidOperationException("Test fatal error");
+    
+    public KafkaLoopGroupConsumerTests()
+    {
+        ILoggerFactory loggerFactory = NullLoggerFactory.Instance;
+
+        var consumerSettings = new List<AbstractConsumerSettings>
+        {
+            new ConsumerSettings
+            {
+                Path = TOPIC
+            }
+        }.AsReadOnly();
+        
+        var mockKafkaConsumer = new Mock<IConsumer<Ignore, byte[]>>();
+        
+        mockKafkaConsumer
+            .Setup(x => x.Consume(It.IsAny<CancellationToken>()))
+            .Throws(_expectedException);
+        
+        var mockProcessorFactory = new Mock<Func<TopicPartition, IKafkaCommitController, IKafkaPartitionConsumer>>();
+        _mockFailureInterceptor = new Mock<IKafkaLoopFailureInterceptor>();
+        
+        var mockConsumerBuilder = new Mock<ConsumerBuilder<Ignore, byte[]>>(new Dictionary<string, string>() );
+        
+        mockConsumerBuilder
+            .Setup(builder => builder.Build()) 
+            .Returns(mockKafkaConsumer.Object);
+        
+        var mockConsumerBuilderFactory = new Mock<Func<ConsumerConfig, ConsumerBuilder<Ignore, byte[]>>>(); 
+        mockConsumerBuilderFactory
+            .Setup(factory => factory(It.IsAny<ConsumerConfig>()))
+            .Returns(mockConsumerBuilder.Object);
+        
+        var settings = new KafkaMessageBusSettings
+        {
+            ConsumerBuilderFactory = mockConsumerBuilderFactory.Object
+        };
+        
+        _subject = new KafkaGroupConsumer(loggerFactory,
+            settings,
+            consumerSettings,
+            [],
+            [_mockFailureInterceptor.Object],
+            GROUP,
+            [TOPIC], mockProcessorFactory.Object);
+    }
+
+    [Fact]
+    public async Task When_LoopExceptionThrown_Then_ShouldIntercept()
+    {
+        await _subject.Start();
+        await Task.Delay(100); 
+
+        await _subject.Stop();
+
+        _mockFailureInterceptor.Verify(
+            x => x.OnFailureAsync(It.Is<KafkaLoopFailureContext>(ctx =>
+                ctx.Group == GROUP && ctx.Exception == _expectedException)),
+            Times.Once,
+            "Expected IKafkaGroupLoopFailureInterceptor.OnFailureAsync to be called once."
+        );
+    }
+
+}
